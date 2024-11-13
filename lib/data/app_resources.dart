1a99a7651c235be422e36f049853b9d1c992388d
Nizlan <nikita.zlobin@list.ru>
2024-11-13 11:54:51 +0300

    added resources

--- lib/data/app_resources.dart
+++ lib/data/app_resources.dart
@@ -1,23 +1,5 @@
 abstract class AppResources {
-  static final List<String> flameImages = [
-    'Polygon 40.png',
-    'Loader.png',
-    'Crystal - Brown 1.png',
-    'Crystal - Yellow.png',
-    'Crystal - Green.png',
-    'Background-Secondary.png',
-    'Btn-Restart 1.png',
-    'Button 1.png',
-    'Btn-Back to menu 1.png',
-    'Btn-Restart.png',
-    'Crystal -Orange.png',
-    'Background 2.png',
-    'Name.png',
-    'Crystal - Blue.png',
-    'Btn-Setting.png',
-    'Crystal - Red.png',
-    'Crystal - Violet.png'
-  ];
+  static final List<String> flameImages = ['Loader.png','Crystal - Brown 1.png','Crystal - Yellow.png','Sidebar (2).png','Crystal - Green.png','music_on.png','tutorial_big.png','Background-Secondary.png','Btn-Restart 1.png','Button 1.png','Btn-Back to menu 1.png','sound_off.png','Btn-Restart.png','tutorial.png','Crystal -Orange.png','music_off.png','Polygon 40.png','close-button.png','Pop-up (1).png','game_over.png','Background 2.png','Name.png','Crystal - Blue.png','Btn-Setting.png','sound_on.png','Crystal - Red.png','Crystal - Violet.png'];
 
   // FLAME_IMAGES_PLACEHOLDER
 
